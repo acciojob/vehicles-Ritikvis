4e361085c1a88c24638fc73e7193e5f4ff58d13b
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-05-06 09:38:36 +0530

    update4

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,5 +3,16 @@ package com.driver.test;
 import com.driver.Boat;
 
 public class TestCases {
-    // Test cases
+    public static void main(String[] args) {
+        // Create a Boat object using the constructor with arguments
+        Boat boat = new Boat("My Boat", 10);
+
+        // Access the name and capacity of the boat using the correct method names
+        String boatName = boat.getName();  // Using getName() instead of getVehicleName()
+        int boatCapacity = boat.getCapacity();  // Using getCapacity() instead of getVehicleCapacity()
+
+        // Print the boat's details
+        System.out.println("Boat Name: " + boatName);
+        System.out.println("Boat Capacity: " + boatCapacity);
+    }
 }
