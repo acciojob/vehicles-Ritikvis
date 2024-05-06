67cc3514e032b48d9ea4245b4032410f0818403b
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-05-06 09:35:38 +0530

    update3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,4 +1,21 @@
 package com.driver;
 
 public class Boat {
+    private String name;
+    private int capacity;
+
+    // Constructor with arguments
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    // Getter methods for name and capacity
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
